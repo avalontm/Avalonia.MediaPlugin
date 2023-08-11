ee9a80a3298e960567254200abcbefeb0cae2eef
Λv∂lΘи ™ <jaimeraulmendezlopez325@hotmail.com>
2023-08-11 15:29:16 -0700

    ios (incomplete)

---
+++ Avalonia.MediaPlugin.iOS/Bootstrapper.cs
@@ -0,0 +1,32 @@
+﻿using Avalonia.MediaPlugin.Abstractions;
+using Avalonia.MediaPlugin.iOS;
+using Splat;
+
+
+namespace Avalonia.MediaPlugin.Android
+{
+    [Preserve(AllMembers = true)]
+    public static class Bootstrapper
+    {
+        public static void Register(IMutableDependencyResolver services, IReadonlyDependencyResolver resolver)
+        {
+            services.Register<IMedia>(() => new MediaImplementation());
+        }
+
+        public static Avalonia.AppBuilder UseMediaPlugin(this Avalonia.AppBuilder builder)
+        {
+            //Registramos 
+            Register(Locator.CurrentMutable, Locator.Current);
+            onPermissions();
+
+            return builder;
+        }
+
+        static void onPermissions()
+        {
+
+           
+
+        }
+    }
+}
---
+++ Avalonia.MediaPlugin.iOS/MediaPickerPopoverDelegate.cs
@@ -0,0 +1,22 @@
+﻿using UIKit;
+
+namespace Avalonia.MediaPlugin.iOS
+{
+    class MediaPickerPopoverDelegate
+         : UIPopoverControllerDelegate
+    {
+        internal MediaPickerPopoverDelegate(MediaPickerDelegate pickerDelegate, UINavigationController picker)
+        {
+            this.pickerDelegate = pickerDelegate;
+            this.picker = picker;
+        }
+
+        public override bool ShouldDismiss(UIPopoverController popoverController) => true;
+
+        public override void DidDismiss(UIPopoverController popoverController) =>
+            pickerDelegate.Canceled(picker);
+
+        readonly MediaPickerDelegate pickerDelegate;
+        readonly UINavigationController picker;
+    }
+}
